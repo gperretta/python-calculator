3cd2258f999721034407487c47729751c731db20
gperretta <gperretta22@fed.idserve.net>
2022-12-27 14:52:20 +0100

    Add class "Calculator". - create calculator class - import tkinter - add class initializer

--- Calculator.py
+++ Calculator.py
@@ -1,2 +1,22 @@
-print("First commit.")
+import tkinter as tk
 
+
+class Calculator:
+
+    # class initializer:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.geometry("390x711")
+        # not resizable:
+        self.window.resizable(0, 0)
+        self.window.title("Calculator")
+
+    # call to Tk main loop:
+    def run(self):
+        self.window.mainloop()
+
+
+# run config.:
+if __name__ == "__main__":
+    calculator = Calculator()
+    calculator.run()
